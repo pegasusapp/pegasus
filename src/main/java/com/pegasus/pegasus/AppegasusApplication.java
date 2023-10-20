4b5e0e2497396e8d76e2b2317695aaa6da18cc60
pegasusapp <oscar.2001601@gmail.com>
2023-10-19 23:05:48 -0500

    add project - first commit

---
+++ src/main/java/com/pegasus/pegasus/AppegasusApplication.java
@@ -0,0 +1,13 @@
+package com.pegasus.pegasus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppegasusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppegasusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pegasus/pegasus/AppegasusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegasus.pegasus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppegasusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
